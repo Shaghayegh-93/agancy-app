590a033c5cd3e1e6582ef02b65e6ff93c725adb7
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-08 14:11:28 +0430

    add circle component for using it in the background of other component

---
+++ components/Circle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Circle = (props) => {
+  return (
+    <div
+      className="h-[100vh] w-[100vh] rounded-full absolute z-50 opacity-10"
+      style={{ ...props }}
+    ></div>
+  );
+};
+
+export default Circle;
