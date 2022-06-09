b6823ef1ce13b8589e4a78cc8d5aadd2e8022752
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-09 13:07:24 +0430

    add mx attribiute

--- components/Circle.jsx
+++ components/Circle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Circle = (props) => {
   return (
     <div
-      className="h-[100vh] w-[100vh] rounded-full absolute z-50 opacity-10"
+      className="h-[100vh] w-[100vh] rounded-full absolute z-50 opacity-10 mx-auto"
       style={{ ...props }}
     ></div>
   );
