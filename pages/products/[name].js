56e02e4d8fa33ee368af6d078c19a597106c203f
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-12 11:09:59 +0430

    change style

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -6,14 +6,14 @@ import { data } from "../../data";
 
 const Product = ({ product }) => {
   return (
-    <div className="grid grid-cols-3  px-[50px] py-[120px]">
-      <div className="grid gap-2 grid-cols-3 col-span-2 mr-8">
+    <div className="grid grid-cols-3  px-[50px] py-[120px] ">
+      <div className="grid gap-2 grid-cols-3 col-span-2 mr-8 ">
         {product.images.map((img) => (
-          <div className=" " key={img.id}>
+          <div className="imgContainer " key={img.id}>
             <Image
               alt=""
               src={img.url}
-              width="250%"
+              width="230%"
               height="200%"
               objectFit="responsive"
               //   layout="fill"
@@ -22,11 +22,11 @@ const Product = ({ product }) => {
         ))}
       </div>
       <div>
-        <div className="">
-          <h2 className="font-bold mb-5 text-2xl">{product.title}</h2>
-          <p className="mb-6">{product.longDesc}</p>
+        <div className="mt-16">
+          <h2 className="font-bold mb-5 text-5xl">{product.title}</h2>
+          <p className="mb-8">{product.longDesc}</p>
           <button
-            className="bg-green-700 p-2 rounded-lg text-white"
+            className="bg-green-700 p-[10px] rounded-lg text-white text-xl cursor-pointer"
             type="button"
           >
             <Link href="/contact">Contact</Link>
