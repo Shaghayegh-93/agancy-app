0f43a3f866c353effb222abb80988cbd23e76efc
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-13 17:26:10 +0430

    lifting state

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 const Layout = ({ children }) => {
+  const [open, setOpen] = useState(true);
+  const menuHandler = () => {
+    setOpen(false);
+  };
   return (
-    <div>
-      <Navbar />
+    <div onClick={menuHandler}>
+      <Navbar open={open} menuHandler={menuHandler} />
       {children}
       <Footer />
     </div>
